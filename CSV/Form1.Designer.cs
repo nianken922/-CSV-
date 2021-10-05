a510255c4c589f8d5c5884fab8a9cf6555535cca
nianken <godken922@gmail.com>
2021-10-05 18:42:13 +0800

    no message

---
+++ CSV/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace CSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnStart2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(32, 33);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(161, 140);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(32, 331);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(161, 140);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(465, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(604, 356);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(298, 33);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(161, 140);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(298, 308);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(161, 140);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnStart2
+            // 
+            this.btnStart2.Location = new System.Drawing.Point(32, 179);
+            this.btnStart2.Name = "btnStart2";
+            this.btnStart2.Size = new System.Drawing.Size(161, 140);
+            this.btnStart2.TabIndex = 5;
+            this.btnStart2.Text = "Start2";
+            this.btnStart2.UseVisualStyleBackColor = true;
+            this.btnStart2.Click += new System.EventHandler(this.btnStart2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(465, 423);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(355, 25);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 483);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnStart2);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnStart2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ CSV/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSV
+{
+    public partial class Form1 : Form
+    {
+        int r;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+            for(int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    r = Mutiply(i, j);
+                    textBox1.Text += i + "*" + j + "=," + r+",,";
+                    if (j == 9)
+                    {
+                        textBox1.Text += Environment.NewLine;
+                    }
+                }
+             }
+        }
+        private void btnStart2_Click(object sender, EventArgs e)
+        {
+            for(int i = 1; i <= 9; i++)
+            {
+                for (int k = 1; k <= 9; k++)
+                {
+                    r = Mutiply(i, k);
+                    textBox1.Text += k + "*" + i + "=," + r + ",,";
+                    if (k == 9)
+                    {
+                        textBox1.Text += Environment.NewLine;
+                    }
+                }
+            }
+        }
+
+        private static int Mutiply(int i, int j)
+        {
+            int r;
+            {
+                r = i * j;
+            }
+            return r;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(@"C:\Users\niahken_wu\source\repos\CSV\test.csv", System.IO.FileMode.Create, System.IO.FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.Default);
+            sw.WriteLine(textBox1.Text);
+            sw.Close();
+            SaveFileDialog saveFile = new SaveFileDialog();
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter writer = new StreamWriter(File.Create(saveFile.FileName));//第二種方式
+                writer.Write(textBox1.Text);
+                writer.Dispose();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+          
+            string[] readtext = System.IO.File.ReadAllLines(@"C:\Users\niahken_wu\source\repos\CSV\test.csv", Encoding.Default);
+            foreach (string s in readtext)
+            {
+                textBox1.Text += s + "\r\n";
+            }
+            OpenFileDialog dialog = new OpenFileDialog();//選擇路徑
+            dialog.Title = "open title";
+            dialog.Filter="Text Files(*.txt)|*.txt|all files(*.*)|*.*";
+            string file="";
+            int size;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader reader = new StreamReader(File.OpenRead(dialog.FileName));
+                textBox1.Text += reader.ReadToEnd();
+                reader.Dispose();
+
+                file = dialog.FileName;
+                string text = File.ReadAllText(file);//顯示完整路徑
+                size = text.Length;
+                textBox2.Text = file;//顯示完整路徑
+                
+                //string[] readtext2= File.ReadAllLines(file, Encoding.Default);
+                //foreach (string q in readtext2)
+                //{
+                //    textBox1.Text += q + "\r\n";
+                //}//相對路徑存取方式        
+            }
+          
+        }
+
+       
+    }
+}
